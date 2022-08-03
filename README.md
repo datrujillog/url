d1dbba3a8059de6212c727dbfe0a3c97c24b0228
yego0210 <diegotrujillo0210@gmail.com>
2022-08-03 01:00:33 -0500

    url

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# url
+# url-shortener
+
+### Installation:
+
+ `$ git clone https://github.com/Ammyy9908/url-shortener.git`
+ 
+ `$ cd url-shortener`
+ 
+ `$ npm i`
+ 
+ `$ npm run dev`
+ 
+ ### Usage
+
+
+open index.js
+and provide the Mongodb Cloud database URL inside mongoose.connect() method inside <DB URL> field.
+
+
+` mongoose.connect('<DB URL>/URLSHORTNER',{useNewUrlParser:true,useUnifiedTopology:true},()=>{
+        console.log("Database connected!");
+    });`
+ 
+ after this go to terminal where this application running see there you see database connected.
+    
+    
+Now goto http://loocalhost:3000 & try to add a long url inside the text field & hit Generate button at the bottom of page you recieve a shorten url just copy it and try to add in new tab of browser you will be redirected to original entered url using this shorten url.
---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const expressLayouts = require('express-ejs-layouts');
+const shortid = require('shortid');
+const cors = require('cors');
+const bp = require('body-parser');
+var validUrl = require('valid-url');
+const URL = require('./models/URL');
+// require('dotenv').config();
+
+
+
+
+const app = express();
+
+
+app.set('view engine','ejs')
+app.use(cors());
+app.use(express.static('public'))
+app.use(bp.json())
+app.use(express.urlencoded({extended: true}));
+
+
+
+
+    mongoose.connect('mongodb+srv://ecommerce:5gEic4TMFr6GTJbC@tienda.bmrbl3h.mongodb.net/URLSHORTNER',{useNewUrlParser:true,useUnifiedTopology:true},()=>{
+        console.log("Database connected!");
+    });
+
+// Database connection
+
+
+
+// Routes
+
+app.get('/',async (req,res) => {
+    res.render('index',{title:'URLY-url shortner'});
+})
+
+app.get('/:lid',async (req,res) => {
+    const url = await URL.findOne({shorten_id:req.params.lid});
+    if(!url){
+        return res.status(500).json({error:"Invalid Shorten URL!"});
+    }
+    res.redirect(url.url);
+})
+app.post('/',async (req,res) => {
+    const {url} = req.body;
+/// first validate the url
+const isValid = validUrl.isUri(url)
+console.log(isValid);
+if(!isValid) {
+    return res.status(400).json({error: 'Invalid URL'});
+}
+//check is there is same url present in database if yes give the shorten id of the url
+   const isExists = await URL.findOne({url:url});
+   if(!isExists) {
+       const newURL = new URL({
+           url,
+           shorten_id:shortid.generate()
+       })
+       const result = await newURL.save();
+       return res.send({shorten_url:`http://localhost:3000/${result.shorten_id}`});
+   }
+   
+})
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port,()=>{
+    
+   
+        console.log(`Server listen at ${port} & DB is also connected!`);
+    
+});
+
+
